1cf052c3913cc04bdd9e41192709a15296492648
Marcos Vinicius <marcos.carini22@gmail.com>
2025-05-13 20:54:23 -0300

    feat: start login page frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+
 import Login from './pages/Login'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import './Login.css';
 
-function Login() {
+const Login = () => {
   return (
-    <h1>Página de Login</h1>
-  )
-}
+    <div className="login-wrapper">
+      <div className="login-box">
+        <h2>Login</h2>
 
-export default Login
+        <form>
+          <label htmlFor="usuario">Usuário</label>
+          <input type="text" id="usuario" placeholder="Digite seu usuário" />
+
+          <label htmlFor="senha">Senha</label>
+          <input type="password" id="senha" placeholder="Digite sua senha" />
+
+          <button type="submit">Entrar</button>
+        </form>
+
+        <p className="signup-text">
+          Não possui uma conta de morador e deseja criar uma?
+          <a href="#" className="signup-link"> Cadastre-se</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
