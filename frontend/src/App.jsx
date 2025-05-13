b6cb2a0aa2301737c9e7bbd05111e52fb11d0d8d
Marcos Vinicius <marcos.carini22@gmail.com>
2025-05-13 17:56:55 -0300

    feat: start frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
